50e871a6922d0e2fa3c88bac70159939e6aba5af
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-09-16 22:38:28 +0430

    network Mediator

---
+++ 03-MediatorPattern/MediatorPattern/Infrastructures/Concretes/Mediator/NetworkMediator.cs
@@ -0,0 +1,29 @@
+﻿using MediatorPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPattern.Infrastructures.Concretes.Mediator;
+
+public class NetworkMediator : IMediator
+{
+    private Dictionary<string,IParticipant> _participants;
+    public NetworkMediator()
+    {
+        _participants = new Dictionary<string,IParticipant>();
+    }
+    public void Register(string key, IParticipant participant)
+    {
+        _participants[key] = participant;
+    }
+
+    public void SendCommand(string receiver, string sender, string command)
+    {
+        if (_participants.ContainsKey(receiver))
+        {
+            _participants[receiver].ReceiveCommand(sender, command);
+        }
+    }
+}
