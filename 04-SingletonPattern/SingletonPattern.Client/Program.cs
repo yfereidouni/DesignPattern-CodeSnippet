78e1888c299a9c5de9b4523999177e1d8bda4eb5
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-09-16 23:16:44 +0430

    Singleton Pattern was developed.

---
+++ 04-SingletonPattern/SingletonPattern.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SingletonPattern;
+
+Console.WriteLine("┌-------------------------------------------------┐");
+Console.WriteLine("|                      Singleton                  |");
+Console.WriteLine("└-------------------------------------------------┘");
+
+
+var object1 =  SingletonObject.GetInstance();
+Console.WriteLine($"Data of object1: {object1.Data}");
+
+var object2 = SingletonObject.GetInstance();
+Console.WriteLine($"Data of object2: {object1.Data}");
+
+Console.WriteLine($"Are objects equal? {object.Equals(object1, object2)}");
+Console.WriteLine($"Are objects reference equal? {object.ReferenceEquals(object1, object2)}");
+
+
+Console.ReadKey();
---
+++ 04-SingletonPattern/SingletonPattern/SingletonObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern;
+
+public class SingletonObject
+{
+	private static SingletonObject? instance;
+	public static SingletonObject GetInstance()
+	{
+		if (instance is null)
+		{
+			var random = new Random();
+			instance = new SingletonObject(random.Next());
+		}
+		return instance;
+	}
+
+	public SingletonObject(int data)
+	{
+		this.Data = data;
+	}
+	public int Data { get; private set; }
+}
